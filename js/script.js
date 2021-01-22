1393163880c1d5eb8795c9746dda2a13063e990a
Mirosław Rup <chrupek999@gmail.com>
2021-01-22 22:06:28 +0100

    blog js step_1

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-/* document.getElementById('test-button').addEventListener('click', function(){
-  const links = document.querySelectorAll('.titles a');
-  console.log('links:', links);
-}); */
+'use strict';
+
 const titleClickHandler = function(event){
-	console.log('Link was clicked!');
-  console.log(event);
+	const clickedElement = this;
+  console.log('Link was clicked!');
 
-	/* remove class 'active' from all article links	*/
-const activeLinks = document.querySelectorAll('.titles a.active');
+	/* [DONE] remove class 'active' from all article links	*/
+	const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+	for(let activeLink of activeLinks){
+  	activeLink.classList.remove('active');
+	}
 
-	/* add class 'active' to the clicked link */
+	/* [IN PROGRESS] add class 'active' to the clicked link */
+	console.log('clickedElement:', clickedElement);
+	clickedElement.classList.add('active');
 
-	/* remove class 'active' from all articles */
-const activeArticles = document.querySelectorAll('.posts article.active');
+	/* [DONE] remove class 'active' from all articles */
+	const activeArticles = document.querySelectorAll('.posts article.active');
 
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
-}
+	for(let activeArticle of activeArticles){
+  	activeArticle.classList.remove('active');
+	}
 
 	/* get 'href' attribute from the clicked link */
 
